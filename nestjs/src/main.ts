c3596be537521ee0f21835b4050270a15fdf6b11
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-07-13 23:43:17 +0900

    7/13 redis&basicAuth

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -2,13 +2,38 @@ import { NestFactory } from '@nestjs/core'
 import { BasicGuard } from './guards/basic.guard'
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './filters/http-exception.filter'
+import * as session from 'express-session'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.useGlobalFilters(new HttpExceptionFilter())
+  // const redisClient = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.REDIS,
+  //     options: {
+  //       db: 0,
+  //       ''
+  //     }
+  //   }
+  // )
 
   app.useGlobalGuards(new BasicGuard())
+
+  app.useGlobalFilters(new HttpExceptionFilter())
+
+  app.use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: true
+      }
+    })
+  )
 
   await app.listen(3000)
 }
